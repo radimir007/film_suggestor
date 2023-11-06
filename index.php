b46502ff49508b21583e8b4620e73a969e6271d8
radimir007 <radovan.psotny123@gmail.com>
2023-11-06 10:33:09 +0100

    Linking styles

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Untitled</title>
+    <link rel="stylesheet" href="_assets/css/normalize.css">
+    <link rel="stylesheet" href="_assets/css/main.css">
 </head>
 <body>
     <h1>Welcome to our website!</h1>
